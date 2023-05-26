b7dc2f54df6d55e2a1a5d8f1b499d298f632d9aa
Alex Stanescu <alex.stanescu86@yahoo.com>
2023-05-26 15:55:25 +0300

    Part 4

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -3,29 +3,51 @@ package Main;
 import Inputs.KeyboardInputs;
 import Inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.Buffer;
+
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
     private float xDelta = 100, yDelta = 100;
-    private float xDir = 1.5f, yDir = 1f;
-
-    private Color color = new Color(150, 20, 90);
-    private Random random;
-
+    private BufferedImage img, subImg;
 
     public GamePanel() {
 
-        random = new Random();
         mouseInputs = new MouseInputs(this);
+
+        importImg();
+
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+
+    }
+
+    private void importImg() {
+
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
 
     }
 
@@ -45,34 +67,9 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        updateRectangle();
-        g.setColor(color);
-        g.fillRect((int) xDelta, (int) yDelta, 200, 50);
+        subImg = img.getSubimage(64, 8*40, 64, 40);
+        g.drawImage(subImg, (int)xDelta, (int)yDelta, 128, 80, null);
 
-
-    }
-
-    private void updateRectangle() {
-        xDelta += xDir;
-        if (xDelta > 400 || xDelta < 0) {
-            xDir *= -1;
-            color = getRandomColor();
-        }
-
-        yDelta += yDir;
-        if (yDelta > 400 || yDelta < 0) {
-            yDir *= -1;
-            color = getRandomColor();
-        }
-
-    }
-
-    private Color getRandomColor() {
-        int r = random.nextInt(255);
-        int g = random.nextInt(255);
-        int b = random.nextInt(255);
-
-        return new Color(r, g, b);
     }
 
 }
--- src/Main/GameWindow.java
+++ src/Main/GameWindow.java
@@ -7,9 +7,11 @@ public class GameWindow extends JFrame {
     public GameWindow(GamePanel gamePanel){
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(400,400);
+
         this.add(gamePanel);
         this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.pack();
         this.setVisible(true);
 
     }
