c71471852c9d6bc7a067b5b7dd50848956c2f15c
Alex Stanescu <alex.stanescu86@yahoo.com>
2023-05-29 21:30:00 +0300

    Part 13.2

--- src/Inputs/MouseInputs.java
+++ src/Inputs/MouseInputs.java
@@ -58,6 +58,11 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseDragged(MouseEvent e) {
+        switch (Gamestate.state) {
+            case PLAYING -> gamePanel.getGame().getPlaying().mouseDragged(e);
+            default -> {
+            }
+        }
 
     }
 
--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -14,7 +14,7 @@ public class Playing extends State implements Statemethods {
     private Player player;
     private LevelManager levelManager;
     private PauseOverlay pauseOverlay;
-    private boolean paused = true;
+    private boolean paused = false;
 
     public Playing(Game game) {
         super(game);
@@ -25,23 +25,27 @@ public class Playing extends State implements Statemethods {
         levelManager = new LevelManager(game);
         player = new Player(200, 200, (int) (64 * Game.SCALE), (int) (40 * Game.SCALE));
         player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
-        pauseOverlay = new PauseOverlay();
+        pauseOverlay = new PauseOverlay(this);
 
     }
 
     @Override
     public void update() {
-        levelManager.update();
-        player.update();
+        if (!paused) {
+            levelManager.update();
+            player.update();
+        } else {
+            pauseOverlay.update();
+        }
 
-        pauseOverlay.update();
     }
 
     @Override
     public void draw(Graphics g) {
         levelManager.draw(g);
         player.render(g);
-        pauseOverlay.draw(g);
+        if (paused)
+            pauseOverlay.draw(g);
     }
 
     @Override
@@ -76,7 +80,7 @@ public class Playing extends State implements Statemethods {
             case KeyEvent.VK_A -> player.setLeft(true);
             case KeyEvent.VK_D -> player.setRight(true);
             case KeyEvent.VK_SPACE -> player.setJump(true);
-            case KeyEvent.VK_BACK_SPACE -> Gamestate.state = Gamestate.MENU;
+            case KeyEvent.VK_ESCAPE -> paused = !paused;
         }
     }
 
@@ -87,6 +91,15 @@ public class Playing extends State implements Statemethods {
             case KeyEvent.VK_D -> player.setRight(false);
             case KeyEvent.VK_SPACE -> player.setJump(false);
         }
+    }
+
+    public void mouseDragged(MouseEvent e){
+        if(paused)
+            pauseOverlay.mouseDragged(e);
+    }
+
+    public void unpauseGame() {
+        paused = false;
     }
 
     public void windowFocusLost() {
